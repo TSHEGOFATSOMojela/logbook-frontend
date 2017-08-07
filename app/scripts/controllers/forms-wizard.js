911891035ac6faee275a626503ce1049c726e511
TSHEGOFATSO <mojelatshego@gmail.com>
2017-08-07 16:09:35 +0200

    admin changes
    completed admin pages

--- app/scripts/controllers/forms-wizard.js
+++ app/scripts/controllers/forms-wizard.js
@@ -1,30 +1,109 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name minovateApp.controller:FormsWizardCtrl
- * @description
- * # FormsWizardCtrl
- * Controller of the minovateApp
- */
-app
-  .controller('FormWizardCtrl', function ($scope) {
-    $scope.page = {
-      title: 'Adding Company'
-    };
-    $scope.company = {};
-    
-    $scope.addCompany= function($scope)
-    {
-        
-       
-        $scope.companies=[];
-        
-        $scope.companies.push($scope.company);
-         $scope.company = {};
-    }
-  });
+//angular.module('app', ['ui.bootstrap','ui.utils']);
 
+app.controller("FormWizardCtrl", function($scope) {
+  $scope.Companies=[
+       {
+            "registrationId": "1",
+            "name": "Skhomotech",
+            "email": "info@skhomotech.co.za",
+            "website": "www.skhomotech.co.za",
+            "address": " 325 Rivonia Blvd, Edenburg, Sandton, 2128",
+            "contacts": "011 053 6076"
+        }
+      
+      
+    ,{
+		   "registrationId": "2",
+            "name": "IT Solution",
+            "email": "info@itSolution.co.za",
+            "website": "www.itSolution.co.za",
+            "address": " 325 Rivonia Blvd, Edenburg, Sandton, 2128",
+            "contacts": "011 053 6076"
+       },
+       {
+		      "registrationId": "3",
+            "name": "IT Solution",
+            "email": "info@itSolution.co.za",
+            "website": "www.itSolution.co.za",
+            "address": " 325 Rivonia Blvd, Edenburg, Sandton, 2128",
+            "contacts": "011 053 6076"
+       },
+         {
+		   "registrationId": "4",
+            "name": "IT Solution",
+            "email": "info@itSolution.co.za",
+            "website": "www.itSolution.co.za",
+            "address": " 325 Rivonia Blvd, Edenburg, Sandton, 2128",
+            "contacts": "011 053 6076"
+       },
+        { 
+          "registrationId": "5",
+            "name": "IT Solution",
+            "email": "info@itSolution.co.za",
+            "website": "www.itSolution.co.za",
+            "address": " 325 Rivonia Blvd, Edenburg, Sandton, 2128",
+            "contacts": "011 053 6076"
+         },
+
+    ]
+
+//
+//$scope.dataTableOpt = {
+//   //custom datatable options
+//  "aLengthMenu": [[10, 50, 100,-1], [10, 50, 100,'All']],
+//  };
+//    
+//    
+//  $scope.selected=[];
+//  $scope.getTemplates=function(Intern){
+//    //some time bellow if will throw expection like $scope.selected[0] is undefined,because on the view you stating on 1 but array always starts on 0
+//   if(Intern[0]===$scope.selected[0]){
+//     return "edit.html";
+//   }else{
+//    return "view.html";
+//   }
+//  };
     
-            
-            
+  
+  $scope.edit=function(u){
+//      $scope.selected = angular.copy(u);
+      $scope.editMode=true;
+      $scope.Companies=angular.copy(u);
+
+  };
+  
+
+      $scope.clear=function(){
+     $scope.editMode=false;
+
+  };
+        $scope.save=function(){
+    if (confirm("are you sure you want to save the changes?") == true) {
+       $scope.Companies=angular.copy(Company);
+  }
+       
+    };
+  $scope.remove = function(n){
+      //NB this will change to "EASY way " once connected to DB
+      if (confirm("are you sure you want to delete") == true) {
+      $scope.Companies.splice(n,1);
+  }
+     
+	};
+
+    //add into a table
+    $scope.addNewCompany=function(newCompany)
+     {
+      
+  
+      var i = $scope.Companies.length;   
+       $scope.Companies[i]=newCompany;
+
+     };
+    
+    
+  
+    
+});
--- app/scripts/controllers/shop-products.js
+++ app/scripts/controllers/shop-products.js
@@ -4,95 +4,105 @@
 
 app.controller("ProductsCtrl", function($scope) {
   $scope.Interns=[
-        [ 
-            1,
-            'tshegofatso',
-            'mojela',
-            'mojelatshego@skhomotech.co.za',
-            'male',
-            'skhomo technologies',
-            'Intern',
-            27726885360
-        ],
-        [
-		    2,
-            'thulani',
-            'malatjie',
-            'malatjie@skhomotech.co.za',
-            'male',
-            'skhomo technologies',
-            'Intern',
-            27726873635
-        ],
-        [
-		3,
-            'naftalie',
-            'morwa',
-            'Nmorwa@skhomotech.co.za',
-            'male',
-            'skhomo technologies',
-            'Intern',
-            27265343434
-        ],
-        [
-		4,
-                  'naftalie',
-            'morwa',
-            'Nmorwa@skhomotech.co.za',
-            'male',
-            'skhomo technologies',
-            'Intern',
-            27265343434
-        ],
-        [
-          5,
-		   'pedi',
-            'malatjie',
-            'malatjie@skhomotech.co.za',
-            'male',
-            'skhomo technologies',
-            'Intern',
-            27726873635
-        ],
+       {
+            "id": "1",
+            "name": "tshegofatso",
+            "surname": "mojela",
+            "email": "mojelatshego@skhomotech.co.za",
+            "gender": "male",
+            "company": "skhomo technologies",
+            "contacts": "27726885360"
+        }
+      
+      
+    ,{
+		    "id": "2",
+            "name": "thulani",
+            "surname": "malatjie",
+            "email": "malatjie@skhomotech.co.za",
+            "gender": "male",
+            "company": "skhomo technologies",
+            "contacts": "27726873635"
+       },
+       {
+		    "id": "3",
+            "name": "naftalie",
+            "surname": "morwa",
+            "email": "Nmorwa@skhomotech.co.za",
+            "gender": "male",
+            "company": "skhomo technologies",
+            "contacts": "27265343434"
+       },
+         {
+		    "id": "4",
+            "name": "fatso",
+            "surname": "kutu",
+            "email": "Fkutu@skhomotech.co.za",
+            "gender": "male",
+            "company": "skhomo technologies",
+            "contacts": "27264735364"
+       },
+        { 
+            "id": "5",
+            "name": "pedi",
+            "surname": "maleka",
+            "email": "pmaleka@skhomotech.co.za",
+            "gender": "shemale",
+            "company": "skhomo technologies",
+            "contacts": "27273456123"
+         },
 
     ]
 
+//
+//$scope.dataTableOpt = {
+//   //custom datatable options
+//  "aLengthMenu": [[10, 50, 100,-1], [10, 50, 100,'All']],
+//  };
+//    
+//    
+//  $scope.selected=[];
+//  $scope.getTemplates=function(Intern){
+//    //some time bellow if will throw expection like $scope.selected[0] is undefined,because on the view you stating on 1 but array always starts on 0
+//   if(Intern[0]===$scope.selected[0]){
+//     return "edit.html";
+//   }else{
+//    return "view.html";
+//   }
+//  };
+    
+  
+  $scope.edit=function(u){
+//      $scope.selected = angular.copy(u);
+      $scope.editMode=true;
+      $scope.Interns=angular.copy(u);
 
-$scope.dataTableOpt = {
-   //custom datatable options
-  "aLengthMenu": [[10, 50, 100,-1], [10, 50, 100,'All']],
-  };
-    
-    
-  $scope.selected=[];
-  $scope.getTemplates=function(Intern){
-    //some time bellow if will throw expection like $scope.selected[0] is undefined,because on the view you stating on 1 but array always starts on 0
-   if(Intern[0]===$scope.selected[0]){
-     return "edit.html";
-   }else{
-    return "view.html";
-   }
-  };
-    
-  
-  $scope.edit=function(u){    
-    $scope.selected=angular.copy(u);
   };
   
-  $scope.save=function(){
-    //custom save function here 
-    //Onces custom save function got success then empty it
-    $.each($scope.Interns,(k,v)=>{
-      if(v[0]===$scope.selected[0]){
-        $scope.Interns[k]=$scope.selected;
-      }
-    });
-     $scope.selected=[];
+// $scope.save=function(){
+//    //custom save function here 
+//    //Onces custom save function got success then empty it
+//    $.each($scope.Interns,(k,v)=>{
+//      if(v[0]===$scope.selected[0]){
+//        $scope.Interns[k]=$scope.selected;
+//      }
+//    });
+//     $scope.selected=[];
+//  };
+//   $scope.clear=function(){
+//    $scope.selected=[];
+//    $scope.editingMode=false;
+//  };
+      $scope.clear=function(){
+     $scope.editMode=false;
+
   };
-  $scope.clear=function(){
-    $scope.selected=[];
-  };
-    
+        $scope.save=function(){
+    if (confirm("are you sure you want to save the changes?") == true) {
+       $scope.Interns=angular.copy(Intern);
+  }
+       
+    };
   $scope.remove = function(n){
       //NB this will change to "EASY way " once connected to DB
       if (confirm("are you sure you want to delete") == true) {
@@ -105,10 +115,10 @@ $scope.dataTableOpt = {
     $scope.addNewIntern=function(newIntern)
      {
       
-         var i = $scope.Interns.length +1;
-         $scope.Interns[i]= Object.entries(newIntern).map((value)=>(value));
-    /*  var i = $scope.Interns.length +1;   
-       $scope.Interns[i]=newIntern;*/
+      //   var i = $scope.Interns.length +1;
+    // $scope.Interns[i]= Object.entries(newIntern).map((value)=>(value));
+      var i = $scope.Interns.length;   
+       $scope.Interns[i]=newIntern
 
      };
     
